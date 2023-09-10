500e765c4939b03d769bfca45b960da3997939c9
RachAppWeb <tryinganything12@gmail.com>
2023-09-10 10:45:17 +0100

    lt commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Testi from "./components/Testi/Testi";
 // import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <>
       <div className="par">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/Testi" element={<Testi />} />
             <Route path="/" element={<Home />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </>
   );
